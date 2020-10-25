57ae50a45f3633c88c830b67e3786cdd6d377868
Lam Si Mon <simon.lam@themobilelife.com>
2020-10-25 22:24:24 +0800

    Initial Commit

---
+++ Geofencing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geofencing
+//
+//  Created by Lam Si Mon on 20-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

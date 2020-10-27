21aef6ccac93b4c6d299b74cff01d944a77fc5ff
Lam Si Mon <simon.lam@themobilelife.com>
2020-10-27 15:20:23 +0800

    - Detect user current location

--- Geofencing/STLLocationViewModel.swift
+++ Geofencing/STLLocationViewModel.swift
@@ -6,5 +6,47 @@
 //
 
 import Foundation
+import Combine
+import CoreLocation
+import UIKit
 
-pub
+public class STLLocationViewModel:NSObject, ObservableObject {
+    
+    private var locationManager = CLLocationManager()
+    
+    @Published var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    @Published var status: CLAuthorizationStatus = CLAuthorizationStatus.notDetermined
+    
+    override public init() {
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+    
+}
+
+extension STLLocationViewModel: CLLocationManagerDelegate {
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        coordinate = location.coordinate
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.status = status
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        
+        guard let identifier = region?.identifier else { return }
+        
+        UIViewController.displayAlert("Monitoring failed with identifier \(identifier)")
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        UIViewController.displayAlert("Location Failed with Error \(error)")
+    }
+}
--- Geofencing/STLMainViewController.swift
+++ Geofencing/STLMainViewController.swift
@@ -7,21 +7,44 @@
 
 import UIKit
 import Combine
+import MapKit
 
-class ViewController: UIViewController {
+class STLMainViewController: UIViewController {
 
     let locationViewModel = STLLocationViewModel()
     private var disposeBag: Set<AnyCancellable> = []
     
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-                
+        setup()
+    }
+
+    private func setup() {
+        
         locationViewModel.$coordinate.receive(on: DispatchQueue.main).sink { coordinate in
             print("Coor 2 \(coordinate)")
         }.store(in: &disposeBag)
+        
+        locationViewModel.$status.receive(on: DispatchQueue.main).sink { status in
+            
+            if status == .authorizedAlways || status == .authorizedWhenInUse {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.zoomToUserLocation()
+                }
+            }
+        }.store(in: &disposeBag)
     }
-
-
+    
+    private func zoomToUserLocation() {
+        mapView.zoomToUserLocation()
+    }
+    
+    @IBAction func onTapCurrentLocation(_ sender: Any) {
+        zoomToUserLocation()
+    }
+    
 }
 

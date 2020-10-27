3abca4c9579fe6104a1d4aff9aa29c6d453db8c5
Lam Si Mon <simon.lam@themobilelife.com>
2020-10-27 20:25:31 +0800

    - Apply swiftformat

--- Geofencing/Model/STLLocationViewModel.swift
+++ Geofencing/Model/STLLocationViewModel.swift
@@ -5,48 +5,44 @@
 //  Created by Lam Si Mon on 20-10-26.
 //
 
-import Foundation
 import Combine
 import CoreLocation
+import Foundation
 import UIKit
 
-public class STLLocationViewModel:NSObject, ObservableObject {
-    
-    private var locationManager = CLLocationManager()
-    
+public class STLLocationViewModel: NSObject, ObservableObject {
+    public var locationManager = CLLocationManager()
+
     @Published var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
-    @Published var status: CLAuthorizationStatus = CLAuthorizationStatus.notDetermined
-    
+    @Published var status = CLAuthorizationStatus.notDetermined
+
     override public init() {
         super.init()
-        
-        self.locationManager.delegate = self
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-    
 }
 
 extension STLLocationViewModel: CLLocationManagerDelegate {
-    
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else {return}
+    public func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
         coordinate = location.coordinate
     }
-    
-    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+
+    public func locationManager(_: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         self.status = status
     }
-    
-    public func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
-        
+
+    public func locationManager(_: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError _: Error) {
         guard let identifier = region?.identifier else { return }
-        
+
         UIViewController.displayAlert("Monitoring failed with identifier \(identifier)")
     }
-    
-    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+
+    public func locationManager(_: CLLocationManager, didFailWithError error: Error) {
         UIViewController.displayAlert("Location Failed with Error \(error)")
     }
 }
